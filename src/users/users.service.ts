af497e902085b267fdbf5adb7fd7ca97525ce16a
web3ten0 <web3ten0@gmail.com>
2023-03-04 17:08:53 +0900

    added prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({ data: createUserInput });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: number) {
